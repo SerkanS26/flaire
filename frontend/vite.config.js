d17802048f74638a7fbeae33e1931ee3d6711d53
SerkanS26 <s.safran84@gmail.com>
2025-02-14 16:58:58 +0100

    target modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,63 +1,68 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import path from "path";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: "/",
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:5000",
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ""),
-//       },
-//     },
-//   },
-
-// });
-
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import dotenv from "dotenv";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+dotenv.config();
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  const API_TARGET =
-    env.NODE_ENV === "development"
-      ? "http://localhost:5000/api"
-      : "https://flaire.safrans.dev/api";
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:5000/api"
+            : "https://flaire.safrans.dev",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-    server: {
-      proxy: {
-        "/api": {
-          target: API_TARGET,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-    },
-    define: {
-      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV),
-    },
-  };
+  },
 });
+
+// import { defineConfig, loadEnv } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import { dirname } from "path";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), "");
+
+//   const API_TARGET =
+//     env.NODE_ENV === "development"
+//       ? "http://localhost:5000/api"
+//       : "https://flaire.safrans.dev";
+
+//   return {
+//     plugins: [react()],
+//     resolve: {
+//       alias: {
+//         "@": path.resolve(__dirname, "./src"),
+//       },
+//     },
+//     server: {
+//       proxy: {
+//         "/api": {
+//           target: API_TARGET,
+//           changeOrigin: true,
+//           rewrite: (path) => path.replace(/^\/api/, ""),
+//         },
+//       },
+//     },
+//     define: {
+//       "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV),
+//     },
+//   };
+// });
