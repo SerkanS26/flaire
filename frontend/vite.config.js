b30706ce2804c5e1894858f7bfb3110c2c4ca649
SerkanS26 <s.safran84@gmail.com>
2025-02-14 19:50:52 +0100

    test 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -28,7 +28,56 @@
 //   },
 // });
 
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import { dirname } from "path";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// export default defineConfig(({ mode }) => {
+//   // const env = loadEnv(mode, process.cwd(), "");
+
+//   // const API_TARGET =
+//   //   mode === "development"
+//   //     ? "http://localhost:5000/api"
+//   //     : "https://flaire.safrans.dev/api";
+
+//   return {
+//     plugins: [react()],
+//     resolve: {
+//       alias: {
+//         "@": path.resolve(__dirname, "./src"),
+//       },
+//     },
+//     server: {
+//       port: 5000,
+//       host: "0.0.0.0", // use this to allow external access
+//       proxy:
+//         mode === "development"
+//           ? {
+//               "/api": {
+//                 target: "http://localhost:5000/api",
+//                 changeOrigin: true,
+//                 rewrite: (path) => path.replace(/^\/api/, ""),
+//               },
+//             }
+//           : undefined, // Disable proxy in production
+//     },
+//     define: {
+//       "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV),
+//     },
+//     // define: {
+//     //   "process.env.API_URL": JSON.stringify(
+//     //     mode === "development" ? "http://localhost:5000/api" : "/api"
+//     //   ),
+//     // },
+//   };
+// });
+
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -38,7 +87,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), "");
 
   const API_TARGET =
     mode === "development"
@@ -54,7 +103,7 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: 5000,
-      host: "0.0.0.0", // use this to allow external access
+      host: "0.0.0.0",
       proxy:
         mode === "development"
           ? {
@@ -66,9 +115,6 @@ export default defineConfig(({ mode }) => {
             }
           : undefined, // Disable proxy in production
     },
-    // define: {
-    //   "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV),
-    // },
     define: {
       "process.env.API_URL": JSON.stringify(API_TARGET),
     },
