c8b33a6d5dad14a5b5e46933b4f9f13b4de41f46
SerkanS26 <s.safran84@gmail.com>
2025-02-14 19:55:01 +0100

    test 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,32 +1,34 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import path from "path";
-// import dotenv from "dotenv";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: "/",
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target:
-//           process.env.NODE_ENV === "development"
-//             ? "http://localhost:5000/api"
-//             : "https://flaire.safrans.dev/api",
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ""),
-//       },
-//     },
-//   },
-// });
+// https://vite.dev/config/
+export default defineConfig({
+  port: 5000,
+  host: "0.0.0.0",
+  base: "/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:5000/api"
+            : "https://flaire.safrans.dev/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
 
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
@@ -76,47 +78,3 @@
 //     // },
 //   };
 // });
-
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  const API_TARGET =
-    mode === "development"
-      ? "http://localhost:5000/api"
-      : "https://flaire.safrans.dev/api";
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    server: {
-      port: 5000,
-      host: "0.0.0.0",
-      proxy:
-        mode === "development"
-          ? {
-              "/api": {
-                target: API_TARGET,
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ""),
-              },
-            }
-          : undefined, // Disable proxy in production
-    },
-    define: {
-      "process.env.API_URL": JSON.stringify(API_TARGET),
-    },
-  };
-});
